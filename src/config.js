d6e43414646ccb1e691b3bb8ff34ebb27c4a3d6a
Nandu <89532571+NanduBit@users.noreply.github.com>
2026-01-09 22:52:00 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,7 +2,7 @@ module.exports = {
   token: "",
   guildID: "1309817795755769926",
   welcomeChannelID: "1387697144990793769",
-  generalChannelID: "1309818552714530856",
+  generalChannelID: "1433112197608771835",
 //  rollID: "1139759856725220422",
   deletedMsgLogServerID: "1397597819841544232",
   deletedMsgLogChannelID: "1434245650043895858",
